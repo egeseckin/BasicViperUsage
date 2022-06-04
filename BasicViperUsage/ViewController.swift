ea37b930c49bfc5cd5474598b42293fbd02061e3
Ege Seçkin <ege.seckin@outlook.com>
2022-06-04 17:57:15 +0300

    Initial Commit

---
+++ BasicViperUsage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicViperUsage
+//
+//  Created by Ege Seçkin on 4.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
